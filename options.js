b38592a86e02e131e70759eee9c5b4c60b7ef584
i-vetrov <i.vetrov@gmail.com>
2012-12-21 12:36:18 -0800

    Create options.js

---
+++ options.js
@@ -0,0 +1,13 @@
+// InvaNode core options
+
+exports.vars = new Object({
+      title: 'InvaNode',
+      appName: 'InvaNode CMS',
+      siteUrl: 'http://www.invanode.org',
+      indexDescription: 'IvaNode - open source blogging content management system based on Node.js',
+      dbHost: '',
+      dbName: '',
+      dbUser: '',
+      dbPass: '',
+      serverListenPort: 80
+});
